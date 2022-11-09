54f216ac6046785f47d52fc3fd88444234c793fa
Kenneth Kimani <kimanim68@gmail.com>
2022-11-09 16:56:47 +0300

    twoscreens

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.twoactivities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mMessageEditText:EditText = findViewById(R.id.editText_send)
+
+        val buttonSend: Button = findViewById(R.id.button_send)
+
+
+
+        buttonSend.setOnClickListener {
+            val secondActivityIntent = Intent( this, SecondActivity::class.java)
+
+            var message = mMessageEditText.text.toString().trim()
+
+            secondActivityIntent.putExtra("SEND_KEY_VALUE", message)
+
+            startActivity(secondActivityIntent)
+
+        }
+
+        val message = intent.getStringExtra("SEND_KEY_VALUE")
+
+        var textView : TextView = findViewById(R.id.text_message)
+
+        textView.text = message
+
+    }
+}
---
+++ app/src/main/java/com/example/twoactivities/SecondActivity.kt
@@ -0,0 +1,45 @@
+package com.example.twoactivities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+        val mMessageEditText:EditText = findViewById(R.id.editText_send)
+
+        val buttonSend: Button = findViewById(R.id.button2)
+
+
+
+        buttonSend.setOnClickListener {
+            val secondActivityIntent = Intent( this, SecondActivity::class.java)
+
+            var message = mMessageEditText.text.toString().trim()
+
+            secondActivityIntent.putExtra("SEND_KEY_VALUE", message)
+
+            startActivity(secondActivityIntent)
+
+        }
+
+
+        val message = intent.getStringExtra("SEND_KEY_VALUE")
+
+        var textView : TextView = findViewById(R.id.text_message)
+
+        textView.text = message
+
+
+
+
+
+    }
+}
